508790f5de1287836d5047bd699e5a3fb4b81f52
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-19 03:44:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 import sys
+from flask import Flask, request
 
 sys.path.append('programs')
-
 
 sys.path.append('bot_analytics')
 import bot_analytics.operator
 
+app = Flask(__name__)
 
-# Loop pemblokiran untuk menjaga kode tetap berjalan
-while True:
-    pass
+@app.route('/')
+def home():
+    return 'Hello, welcome to my Flask app!'
+
+@app.route('/echo', methods=['POST'])
+def echo():
+    data = request.json  # Mendapatkan data JSON dari permintaan POST
+    if data:
+        message = data.get('message', 'No message received!')
+        return f"Received message: {message}"
+    else:
+        return 'No JSON data received!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8000)
+
   
