cb9ceda441f2257f4e334a4dfc0e8399bd96ac78
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-18 21:32:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,13 @@
-import telepot
-from telepot.loop import MessageLoop
-from g4f.client import Client
+import sys
+
+sys.path.append('programs')
 
 
-client = Client()
-def handle_message(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    if content_type == 'text':
-        response = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[{"role": "user", "content": "Hello"}]
-        )
-        
-        bot.sendMessage(chat_id, response.choices[0].message.content)
+sys.path.append('bot_analytics')
+import bot_analytics.main
 
-TOKEN = '6653553439:AAH6C_O5fWysqxZchHDE-sRHsxBtTy2dGTs'
-bot = telepot.Bot(TOKEN)
-MessageLoop(bot, handle_message).run_as_thread()
 
-print('Bot sedang berjalan...')
-
+# Loop pemblokiran untuk menjaga kode tetap berjalan
 while True:
     pass
   
