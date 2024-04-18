e018bc83cde0cfe483baf39b87da7e631ffb4d57
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-19 05:46:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ if __name__ == '__main__':
     TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
     bot = telepot.Bot(TOKEN)
     MessageLoop(bot, {'chat': handle_message}).run_as_thread()
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000)
