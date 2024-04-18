7c4d8990691a1eb54c8677b2cfe19c38fcfc93f8
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-19 05:13:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,15 +12,6 @@ app = Flask(__name__)
 def home():
     return 'Hello, welcome to my Flask app!'
 
-@app.route('/echo', methods=['POST'])
-def echo():
-    data = request.json  # Mendapatkan data JSON dari permintaan POST
-    if data:
-        message = data.get('message', 'No message received!')
-        return f"Received message: {message}"
-    else:
-        return 'No JSON data received!'
-
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
 
