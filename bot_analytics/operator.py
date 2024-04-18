a8faee86421ce33c30af179f13f52ff09505b965
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-19 05:35:48 +0700

    Update operator.py

--- bot_analytics/operator.py
+++ bot_analytics/operator.py
@@ -1,26 +1,20 @@
+import os
+from tinydb import TinyDB
+from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 from programs.gpt import gpt3
 import telepot
-from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
-from telepot.loop import MessageLoop
-import os
-from tinydb import TinyDB, Query
-
 
 TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
 bot = telepot.Bot(TOKEN)
 db = TinyDB('chat_data.json')
 
-
 def handle_message(msg):
-  content_type, chat_type, chat_id = telepot.glance(msg)
-  if content_type == 'text':
-    message = msg['text']
-    response = gpt3(message)
-    db.insert({'chat_id': chat_id, 'question': message, 'answer': response})
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    if content_type == 'text':
+        message = msg['text']
+        response = gpt3(message)
+        db.insert({'chat_id': chat_id, 'question': message, 'answer': response})
 
-        # 
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Regenerate', callback_data='regenerate')]])
-    bot.sendMessage(chat_id, response, reply_markup=keyboard)
-
-
-MessageLoop(bot, {'chat': handle_message}).run_as_thread()
+        keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Regenerate', callback_data='regenerate')]])
+        bot.sendMessage(chat_id, response, reply_markup=keyboard)
+ 
