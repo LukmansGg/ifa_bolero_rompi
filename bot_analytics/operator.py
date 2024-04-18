83eb46dd455ab6b99ca7366d79c967bc7f258f92
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-19 05:15:02 +0700

    Update operator.py

--- bot_analytics/operator.py
+++ bot_analytics/operator.py
@@ -22,17 +22,5 @@ def handle_message(msg):
     keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Regenerate', callback_data='regenerate')]])
     bot.sendMessage(chat_id, response, reply_markup=keyboard)
 
-def handle_callback(msg):
-  query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
-
-  # Temukan data pertanyaan yang sesuai dalam database
-  Question = Query()
-  result = db.search((Question.chat_id == from_id) & (Question.answer == query_data))
-
-  if result:
-      original_message_id = result[0]['message_id']
-      response = gpt3(Question.question)
-      bot.editMessageText((from_id, original_message_id), response)
-
 
 MessageLoop(bot, {'chat': handle_message, 'callback_query': handle_callback}).run_as_thread()
