0d67db07c4f24b6fb70fc12f5b3e18a8c8e39346
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-19 05:34:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import sys
 from flask import Flask, request
+import telepot
+from telepot.loop import MessageLoop
+from bot_analytics.operator import handle_message
 
 sys.path.append('programs')
-
-sys.path.append('bot_analytics')
-import bot_analytics.operator
 
 app = Flask(__name__)
 
@@ -13,9 +13,7 @@ def home():
     return 'Hello, welcome to my Flask app!'
 
 if __name__ == '__main__':
+    TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
+    bot = telepot.Bot(TOKEN)
+    MessageLoop(bot, {'chat': handle_message}).run_as_thread()
     app.run(debug=True, host='0.0.0.0', port=8000)
-
-# Loop pemblokiran untuk menjaga kode tetap berjalan
-while True:
-    pass
-  
