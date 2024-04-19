89bb7cc77c599f6a70477b6a2af0a12c2299c2b2
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-19 13:44:37 +0700

    Update tanya.py

--- bot_analytics/commands/tanya.py
+++ bot_analytics/commands/tanya.py
@@ -2,6 +2,7 @@ import telepot
 from telepot.namedtuple import InlineKeyboardMarkup, ReplyKeyboardMarkup, InlineKeyboardButton, KeyboardButton
 from telepot.exception import TelegramError
 
+from programs.gpt import gpt3
 
 TOKEN = '6569008899:AAF3DCqPjVg2Sgrr6lH8UDGawuZkB8psF1M'
 bot = telepot.Bot(TOKEN)
@@ -12,5 +13,6 @@ def command_handler(sent_message, message):
 
     message_id = sent_message.get('message_id')
     chat_id = sent_message.get('chat_id')
-    
-    bot.editMessageText((chat_id, message_id), answer)
+    regenerate = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Re-generate', callback_data = 'regenerate')]])
+    bot.editMessageText((chat_id, message_id), answer, reply_markup=regenerate)
+    db.insert({'chat_id': chat_id, 'message_id': message_id, 'question': message, 'answer': response})
