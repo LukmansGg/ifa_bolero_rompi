e0d895647cdda536fb96e60bec398211b26fc9fd
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-20 05:26:56 +0700

    Update start.py

--- bot_analytics/commands/start.py
+++ bot_analytics/commands/start.py
@@ -1,7 +1,7 @@
 import telepot
 from tinydb import TinyDB
 from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
-from bot_analytics.command import TELEGRAM_COMMANDS
+from bot_analytics.command import TELEGRAM_BOT_COMMANDS
 from config import TOKEN
 
 bot = telepot.Bot(TOKEN)
