be9ce71f247b77bd06d394acfb623d60432fd7f9
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-19 13:36:18 +0700

    Update message.py

--- programs/message.py
+++ programs/message.py
@@ -11,3 +11,6 @@ def answeringMessage(chat_id, message, response):
     sent_message = bot.sendMessage(chat_id, response, reply_markup=keyboard)
     message_id = sent_message.get('message_id')
     db.insert({'chat_id': chat_id, 'message_id': message_id, 'question': message, 'answer': response})
+
+def regenerate_answer():
+    return "Wip"
