cd5171ac5ec2f63a1e31bce24d6417acaab96e41
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-19 20:00:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import importlib
 from flask import Flask, request
 import telepot
 from telepot.loop import MessageLoop
-from bot_analytics.operator import handle_message
+from bot_analytics.operator import handle_message, TOKEN
 
 sys.path.append('programs')
 
@@ -15,7 +15,6 @@ def home():
     return 'Hello, welcome to my Flask app!'
 
 if __name__ == '__main__':
-    TOKEN = '6745240094:AAE0s6qclbYVCpSuCCCZHl5Y3wswqTT72fc'
     bot = telepot.Bot(TOKEN)
-    MessageLoop(bot, {'chat': handle_message}).run_as_thread()
+    MessageLoop(bot, {'chat': handle_message, 'callback_query': handle_callback}).run_as_thread()
     app.run(host='0.0.0.0', port=8000)
