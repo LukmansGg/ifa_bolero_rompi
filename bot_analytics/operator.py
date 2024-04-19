8186b0ecd52e39aa6762a5a9283a45cb56a131d2
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-19 13:10:12 +0700

    Update operator.py

--- bot_analytics/operator.py
+++ bot_analytics/operator.py
@@ -1,8 +1,10 @@
 import os
 from tinydb import TinyDB
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+
 from programs.gpt import gpt3
 import telepot
+from programs.message import answeringMessage
 
 TOKEN = '6569008899:AAF3DCqPjVg2Sgrr6lH8UDGawuZkB8psF1M'
 bot = telepot.Bot(TOKEN)
@@ -12,9 +14,13 @@ def handle_message(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     if content_type == 'text':
         message = msg['text']
-        response = gpt3(message)
-        db.insert({'chat_id': chat_id, 'question': message, 'answer': response})
-
-        keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Regenerate', callback_data='regenerate')]])
-        bot.sendMessage(chat_id, response, reply_markup=keyboard)
+        for command in TELEGRAM_COMMANDS.keys():
+            if command in message:
+                module_name = TELEGRAM_COMMANDS[command]
+                module = importlib.import_module(module_name)
+                # runing command class
+                module.handle_command()
+            else:
+                answer = gpt3(message)
+                answeringMessage(chat_id, message_id, message, answer)
  
