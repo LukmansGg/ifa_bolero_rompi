c47f090376271356779478f4392d784b5859464f
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-19 19:54:48 +0700

    Update start.py

--- bot_analytics/commands/start.py
+++ bot_analytics/commands/start.py
@@ -1,24 +1,17 @@
 import telepot
 from tinydb import TinyDB
 from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
-from telepot.exception import TelegramError
+from bot_analytics.command import TELEGRAM_COMMANDS
 
-TOKEN = '6745240094:AAE0s6qclbYVCpSuCCCZHl5Y3wswqTT72fc'
+TOKEN = 'your_bot_token_here'
 bot = telepot.Bot(TOKEN)
 db = TinyDB('chat_data.json')
 
-def command_handler(bot_message, msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    sent_message = telepot.message_identifier(bot_message)
-    user_message = telepot.message_identifier(message)
-    message = msg['text']
+def command_handler(sent_message, message):
+    chat_id = sent_message['chat']['id']
+    message_id = sent_message['message_id']
 
-    bot.sendMessage(chat_id, "Pilih salah satu materi disini👇.")
-    
-    
-    bot.sendMessage(chat_id, f'Pesan dari bot: Chat ID Anda adalah {chat_id}, dan message ID Anda adalah {message_id} dan {user_message_id}')
-
-    bot.editMessageText(sent_message, "Selamat Datang di @Ifa_bolero_dan_rompi_bot\nDisini kita dapat belajar bersama berbagai Hal tentang Bolero/Rompi😁👍\n", reply_markup=ReplyKeyboardMarkup(
+    bot.editMessageText((chat_id, message_id), "Selamat Datang di @Ifa_bolero_dan_rompi_bot\nDisini kita dapat belajar bersama berbagai Hal tentang Bolero/Rompi😁👍\n", reply_markup=ReplyKeyboardMarkup(
         keyboard=[
             [KeyboardButton(text="Pengertian✍️️")],
             [KeyboardButton(text="Menyiapkan Ukuran📏")],
@@ -26,11 +19,12 @@ def command_handler(bot_message, msg):
             [KeyboardButton(text="Bahan🧵")],
             [KeyboardButton(text="Vidio Tutorial⏸")]
         ],
-        resize_keyboard= True
+        resize_keyboard=True
     ))
 
-    # Hapus pesan yang dikirim oleh pengguna
+    bot.sendMessage(chat_id, "Pilih salah satu materi disini👇.")
     try:
-        bot.deleteMessage(user_message)
-    except TelegramError as e:
+        user_message_id = message.get('message_id')
+        bot.deleteMessage((chat_id, user_message_id))
+    except telepot.exception.TelegramError as e:
         pass
