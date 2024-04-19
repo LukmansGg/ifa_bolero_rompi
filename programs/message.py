c31e519f8649d6dcd68f4f9e82f7f6d49a1c4180
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-19 20:38:01 +0700

    Update message.py

--- programs/message.py
+++ programs/message.py
@@ -6,7 +6,10 @@ from config import TOKEN
 bot = telepot.Bot(TOKEN)
 db = TinyDB('chat_data.json')
 
-def answeringMessage(chat_id, message, response):
+def answeringMessage(bot_message, chat_id, message, response):
+    bot_chat_id = bot_message['chat']['id']
+    bot_message_id = bot_message['message_id']
+    
     regenerate = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Re-generate', callback_data = 'regenerate')]])
     sent_message = bot.sendMessage(chat_id, response, reply_markup=regenerate)
     message_id = sent_message.get('message_id')
