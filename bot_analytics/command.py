cf22b76b20cd4fc2215ebfccc39525ec4d778d88
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-19 21:08:00 +0700

    Update command.py

--- bot_analytics/command.py
+++ bot_analytics/command.py
@@ -1,6 +1,6 @@
 import importlib
 
-TELEGRAM_BOTS_COMMANDS = {
+TELEGRAM_BOT_COMMANDS = {
     "/start": "bot_analytics.commands.start",
     "/tanya": "bot_analytics.commands.tanya"
     #"/cari": "bot_analytics.commands.cari"
