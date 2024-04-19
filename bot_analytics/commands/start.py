0381446e191a5ff404f7ddd8990f299dbb62ff95
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-19 16:50:33 +0700

    Update start.py

--- bot_analytics/commands/start.py
+++ bot_analytics/commands/start.py
@@ -3,7 +3,7 @@ from tinydb import TinyDB
 from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
 from telepot.exception import TelegramError
 
-TOKEN = '6569008899:AAGzHuBX5JuyHx1ijAzWzjKPhsq6NOPAL3o'
+TOKEN = '6569008899:AAGkouDkdodOrx9vIBQGQjwvW7H8XIOk5d8'
 bot = telepot.Bot(TOKEN)
 db = TinyDB('chat_data.json')
 
@@ -11,6 +11,11 @@ def command_handler(sent_message, message):
     message_id = sent_message.get('message_id')
     chat_id = sent_message.get('chat_id')
     
+    bot.sendMessage(chat_id, "Pilih salah satu materi disini👇.")
+    user_message_id = message.get('message_id')
+    
+    bot.sendMessage(chat_id, f'Pesan dari bot: Chat ID Anda adalah {chat_id}, dan message ID Anda adalah {message_id} dan {user_message_id}')
+
     bot.editMessageText((chat_id, message_id), "Selamat Datang di @Ifa_bolero_dan_rompi_bot\nDisini kita dapat belajar bersama berbagai Hal tentang Bolero/Rompi😁👍\n", reply_markup=ReplyKeyboardMarkup(
         keyboard=[
             [KeyboardButton(text="Pengertian✍️️")],
@@ -21,11 +26,9 @@ def command_handler(sent_message, message):
         ],
         resize_keyboard= True
     ))
-    bot.sendMessage(chat_id, "Pilih salah satu materi disini👇.")
-    
+
     # Hapus pesan yang dikirim oleh pengguna
     try:
-        user_message_id = message.get('message_id')
         bot.deleteMessage((chat_id, user_message_id))
     except TelegramError as e:
         pass
