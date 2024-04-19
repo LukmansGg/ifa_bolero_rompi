af12e9eeb70ba60616942398ccc993c7bb5673d6
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-19 16:34:24 +0700

    Update start.py

--- bot_analytics/commands/start.py
+++ bot_analytics/commands/start.py
@@ -3,7 +3,7 @@ from tinydb import TinyDB
 from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
 from telepot.exception import TelegramError
 
-TOKEN = '6569008899:AAFgs9mO41_8F4O5gkg7CpCweXmTgx2TvWE'
+TOKEN = '6569008899:AAGzHuBX5JuyHx1ijAzWzjKPhsq6NOPAL3o'
 bot = telepot.Bot(TOKEN)
 db = TinyDB('chat_data.json')
 
