8cae92e66ba96ce92acdf1371910ce075889aea2
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-19 13:14:29 +0700

    Update message.py

--- programs/message.py
+++ programs/message.py
@@ -1,2 +1,8 @@
 
-def 
+
+
+def answeringMessage(chat_id, message, response):
+    regenerate = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Re-generate', callback_data = 'regenerate')]])
+    sent_message = bot.sendMessage(chat_id, response, reply_markup=keyboard)
+    message_id = sent_message.get('message_id')
+    db.insert({'chat_id': chat_id, 'message_id': message_id, 'question': message, 'answer': response})
