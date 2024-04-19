57a4d72189a0bd815f8f7d6aea69a3dea019192e
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-19 13:09:21 +0700

    Update command.py

--- bot_analytics/command.py
+++ bot_analytics/command.py
@@ -1,10 +1,10 @@
+import importlib
 
 TELEGRAM_COMMANDS = {
     "start": "commands/start.py",
     "tanya": "commands/tanya.py",
     "cari": "commands/cari.py"
 }
-
 
 def run_command:
     return "Wip"
