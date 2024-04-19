b6d7d7beb28249fa57b6289750994737625e0483
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-19 13:42:41 +0700

    Update tanya.py

--- bot_analytics/commands/tanya.py
+++ bot_analytics/commands/tanya.py
@@ -1,4 +1,16 @@
+import telepot
+from telepot.namedtuple import InlineKeyboardMarkup, ReplyKeyboardMarkup, InlineKeyboardButton, KeyboardButton
+from telepot.exception import TelegramError
+
+
+TOKEN = '6569008899:AAF3DCqPjVg2Sgrr6lH8UDGawuZkB8psF1M'
+bot = telepot.Bot(TOKEN)
+db = TinyDB('chat_data.json')
 
 def command_handler(sent_message, message):
     answer = gpt3(message)
-    answeringMessage(chat_id, message, answer)
+
+    message_id = sent_message.get('message_id')
+    chat_id = sent_message.get('chat_id')
+    
+    bot.editMessageText((chat_id, message_id), answer)
