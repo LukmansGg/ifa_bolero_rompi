26a8f61688fb17d110cc0c1bba7b7a6c03f988dc
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-19 13:59:57 +0700

    Update message.py

--- programs/message.py
+++ programs/message.py
@@ -2,13 +2,13 @@ from tinydb import TinyDB
 import telepot
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 
-TOKEN = '6569008899:AAG9RZMt321uPW0LWJMAFk509DPoK9SRWc4'
+TOKEN = '6569008899:AAGKZ6ZAi1d_JDYCDZJ9vaGZVi3pItlw56w'
 bot = telepot.Bot(TOKEN)
 db = TinyDB('chat_data.json')
 
 def answeringMessage(chat_id, message, response):
     regenerate = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Re-generate', callback_data = 'regenerate')]])
-    sent_message = bot.sendMessage(chat_id, response, reply_markup=keyboard)
+    sent_message = bot.sendMessage(chat_id, response, reply_markup=regenerate)
     message_id = sent_message.get('message_id')
     db.insert({'chat_id': chat_id, 'message_id': message_id, 'question': message, 'answer': response})
 
