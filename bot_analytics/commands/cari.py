0a50f4e3e9d518e46b2da63be276b621b2441499
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-20 05:28:23 +0700

    Update cari.py

--- bot_analytics/commands/cari.py
+++ bot_analytics/commands/cari.py
@@ -1,1 +1,16 @@
 from programs.search import search
+import telepot
+from tinydb import TinyDB
+from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
+from bot_analytics.command import TELEGRAM_COMMANDS
+from config import TOKEN
+
+bot = telepot.Bot(TOKEN)
+db = TinyDB('chat_data.json')
+
+def command_handler(sent_message, message):
+    chat_id = sent_message['chat']['id']
+    message_id = sent_message['message_id']
+    
+    try:
+        bot.editMessageText((chat_id, message_id),
