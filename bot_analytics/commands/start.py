b86d76e2a31c3dc300934152d99782136956ebbd
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-19 13:38:22 +0700

    Update start.py

--- bot_analytics/commands/start.py
+++ bot_analytics/commands/start.py
@@ -2,10 +2,11 @@ import telepot
 from telepot.namedtuple import InlineKeyboardMarkup, ReplyKeyboardMarkup, InlineKeyboardButton, KeyboardButton
 from telepot.exception import TelegramError
 
-def command_handler(bot,display, message):
+def command_handler(bot,sent_message, message):
+    message_id = sent_message.get('message_id')
+    chat_id = sent_message.get('chat_id')
     
-    
-    bot.sendMessage(chat_id, "Selamat Datang di @Ifa_bolero_dan_rompi_bot\nDisini kita dapat belajar bersama berbagai Hal tentang Bolero/Rompi😁👍\n", reply_markup=ReplyKeyboardMarkup(
+    bot.editMessageText((chat_id, message_id), "Selamat Datang di @Ifa_bolero_dan_rompi_bot\nDisini kita dapat belajar bersama berbagai Hal tentang Bolero/Rompi😁👍\n", reply_markup=ReplyKeyboardMarkup(
         keyboard=[
             [KeyboardButton(text="Pengertian✍️️")],
             [KeyboardButton(text="Menyiapkan Ukuran📏")],
