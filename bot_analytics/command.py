f7c31c0a1175a598567efa2e140a4902ae9ed7f1
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-20 06:38:44 +0700

    Update command.py

--- bot_analytics/command.py
+++ bot_analytics/command.py
@@ -3,7 +3,7 @@ import importlib
 TELEGRAM_BOT_COMMANDS = {
     "/start": "bot_analytics.commands.start",
     "/tanya": "bot_analytics.commands.tanya"
-    #"/cari": "bot_analytics.commands.cari"
+    "/cari": "bot_analytics.commands.cari"
 }
 
 def addCommand():
