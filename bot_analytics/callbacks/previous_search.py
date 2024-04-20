641a6dba7abfdc5e67633a13c4f0957097152e25
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-20 18:12:45 +0700

    Update callbacks

---
+++ bot_analytics/callbacks/previous_search.py
@@ -0,0 +1,34 @@
+from tinydb import TinyDB, Query
+from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+from config import TOKEN
+import telepot
+from programs.message import editMessage
+
+bot = telepot.Bot(TOKEN)
+chat_db = TinyDB('chat_data.json')
+search_db = TinyDB('search_results.json')
+
+
+def callback_handler(msg):
+    query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
+    chat_message_id = msg['message']['message_id']
+    callback_id = msg['id']
+
+    query = Query()
+    chat_result = chat_db.get(query.message_id == chat_message_id)
+    if chat_result:
+        message_search_id = chat_result['search_id']
+        search_result = search_db.get(query.search_id == message_search_id)
+        if search_result:
+            next_page_value = chat_result['link_page'] - 1 if chat_result['link_page'] > 0 else 0
+            link = search_result['links'][next_page_value]
+            title = link['title']
+            description = link['description']
+            url = link['link']
+            
+            keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='»Next»', callback_data='searchnext')], [InlineKeyboardButton(text='«Previous«', callback_data='searchback')]])
+            editMessage(from_id, chat_message_id, f"<b>[{next_page_value}/10] Hasil Pencarian: [{title}]</b>\n\n{description}\nsumber: {url}\n\npowered by [googlesearch]", reply_markup=keyboard)
+            chat_db.update({'link_page': next_page_value}, query.message_id == chat_message_id)
+            
+    else:
+        bot.answerCallbackQuery(callback_id, "Tidak Dapat Menemukan Pesan")
