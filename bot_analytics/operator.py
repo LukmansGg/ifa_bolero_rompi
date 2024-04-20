6ff9084143a6fc3b9cf3b4ef8375756342df2115
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-20 10:36:17 +0700

    Update operator.py

--- bot_analytics/operator.py
+++ bot_analytics/operator.py
@@ -41,7 +41,7 @@ def handle_message(msg):
 
             bot_message = bot.sendMessage(chat_id, "tunggu...")
             answer = gpt3(message + " bisakah membantu saya, apa fungsi /start, /tanya, dan /cari untuk menjalankan materi busana bolero dan rompi ini")
-            answeringMessage(bot_message, chat_id, message, answer)
+            answeringMessage(bot_message, chat_id, message + " bisakah membantu saya, apa fungsi /start, /tanya, dan /cari untuk menjalankan materi busana bolero dan rompi ini", answer)
 
  
 def handle_callback(msg):
