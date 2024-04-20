5004db387f0a34b7d7638111216d150e919f2e19
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-20 07:10:17 +0700

    Update gpt.py

--- programs/gpt.py
+++ programs/gpt.py
@@ -5,7 +5,10 @@ gpt_client = Client()
 def gpt3(prompt):
   try:
     response = gpt_client.chat.completions.create(model='gpt-3.5-turbo', messages=[{'role': 'user', 'content': prompt}])
-    return response.choices[0].message.content
+    if response.choices[0].message.content == "当前地区当日额度已消耗完, 请尝试更换网络环境":
+        return "Maaf sedang terjadi kesalahan pada sistem ai kami, silakan coba lagi"
+    else:
+        return response.choices[0].message.content
   except Exception as e:
     return "Terjadi kesalahan: {}".format(e)
 
