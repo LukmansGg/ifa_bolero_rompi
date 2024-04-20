400a91ed222bc40195e6e153dd38dc29a5d21305
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-20 14:03:28 +0700

    Update callback.py

--- bot_analytics/callback.py
+++ bot_analytics/callback.py
@@ -1,5 +1,7 @@
 TELEGRAM_BOT_CALLBACKS = {
     "regenerate": "bot_analytics.callbacks.regenerate_text",
     "sejarah": "bot_analytics.callbacks.materi_page_sejarah",
-    "pengertian": "bot_analytics.callbacks.materi_page_pengertian"
+    "pengertian": "bot_analytics.callbacks.materi_page_pengertian",
+    perbedaan": "bot_analytics.callbacks.materi_page_sejarah",
+    "desain": "bot_analytics.callbacks.materi_page_pengertian"
 }
