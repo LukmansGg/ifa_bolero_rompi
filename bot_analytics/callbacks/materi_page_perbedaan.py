4f63973696b62eff44dc1a856c287e928ef4f2bb
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-20 17:22:26 +0700

    Update materi_page_perbedaan.py

--- bot_analytics/callbacks/materi_page_perbedaan.py
+++ bot_analytics/callbacks/materi_page_perbedaan.py
@@ -20,7 +20,7 @@ def callback_handler(msg):
     query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
     original_message_id = msg['message']['message_id']
 
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='« Back', callback_data = 'home_pengertian')]])
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='« Back', callback_data = 'homepengertian')]])
 
     question = 'jelaskan Perbedaan Pakaian Bolero dan Rompi dari segi desain, asal usul, proses pembuatan, dan bahan, sertakan juga sumber website nya'
     response = gpt3(question)
