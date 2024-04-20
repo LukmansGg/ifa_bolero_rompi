01a0dbd20db78a9f38e82e191b58e7d5833784e3
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-20 14:55:40 +0700

    Update materi_page_pengertian.py

--- bot_analytics/callbacks/materi_page_pengertian.py
+++ bot_analytics/callbacks/materi_page_pengertian.py
@@ -20,7 +20,7 @@ def callback_handler(msg):
     query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
     original_message_id = msg['message']['message_id']
 
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Regenerate', callback_data = 'regenerate')]])
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Re-Generate', callback_data = 'regenerate')],[InlineKeyboardButton(text='« Back', callback_data = 'home_pengertian')]])
 
     question = 'jelaskan materi pengertian bolero dan rompi, baik dari perbedaan, asal usul, desain, dll'
     response = gpt3(question)
