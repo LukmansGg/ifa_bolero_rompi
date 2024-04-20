ac25ae08233409950d331e7c1102c9c171b67d98
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-20 21:46:53 +0700

    Update materi_page_pengertian.py

--- bot_analytics/callbacks/materi_page_pengertian.py
+++ bot_analytics/callbacks/materi_page_pengertian.py
@@ -1,30 +1,43 @@
-import os
-import sys
-import importlib
 from tinydb import TinyDB, Query
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
-
+from programs.search import searching  # Pastikan untuk mengimpor searching
+import random
 from config import TOKEN
 from programs.gpt import gpt3
 import telepot
-from programs.message import answeringMessage
-from bot_analytics.command import TELEGRAM_BOT_COMMANDS
-
-#sys.path.append('commands')
 
 bot = telepot.Bot(TOKEN)
 db = TinyDB('chat_data.json')
+search_db = TinyDB('search_results.json')  # Definisikan search_db
 
 
 def callback_handler(msg):
     query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
     original_message_id = msg['message']['message_id']
 
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Re-Generate', callback_data = 'pengertian')],[InlineKeyboardButton(text='« Back', callback_data = 'homepengertian')]])
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text='Re-Generate', callback_data='pengertian')],
+        [InlineKeyboardButton(text='« Back', callback_data='homepengertian')]
+    ])
 
     question = 'jelaskan materi pengertian bolero dan rompi, baik dari perbedaan, asal usul, desain, dll'
     response = gpt3(question)
     # Edit pesan asli dengan respon
-    bot.editMessageText((from_id, original_message_id), f'-<b>Pengertian Bolero dan Rompi</b>-\n\nMenurut beberapa sumber:\n[ <b>URL</b> ]\n\n{response}', parse_mode="HTML", reply_markup=keyboard)
+
+    result = searching("pengertian busana bolero dan rompi")
+    query = Query()
+    get_result = search_db.get(query.search_id == result)
+
+    if get_result:
+        links = get_result.get('links', [])
+        if links:
+            random_index = random.randint(0, min(9, len(links) - 1))  # Pastikan indeks tidak melebihi panjang list
+            link = links[random_index]
+            url = link['link']
+            bot.editMessageText((from_id, original_message_id), f'<b>Pengertian Bolero dan Rompi</b>\n\nMenurut beberapa sumber:\n[ <b>{url}</b> ]\n\n{response}', parse_mode="HTML", reply_markup=keyboard)
+        else:
+            bot.editMessageText((from_id, original_message_id), f'<b>Pengertian Bolero dan Rompi</b>\n\nMenurut beberapa sumber:\n[ <b>https://kumparan.com/hijab-lifestyle/mengulik-sejarah-rompi-yang-sudah-populer-sejak-abad-ke-17-1y1JZmTp0ZL</b> ]\n\n{response}', parse_mode="HTML", reply_markup=keyboard)
+    else:
+        bot.editMessageText((from_id, original_message_id), f'<b>Pengertian Bolero dan Rompi</b>\n\nMenurut beberapa sumber:\n[ <b>https://www.womanindonesia.co.id/sejarah-jaket-bolero-di-industri-fashion/</b> ]\n\n{response}', parse_mode="HTML", reply_markup=keyboard)
+
     db.insert({'chat_id': from_id, 'message_id': original_message_id, 'question': question, 'answer': response})
-  
