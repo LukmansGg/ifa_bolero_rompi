d41b10c689c23d9b60598f9860096a15bbd2201d
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-20 10:17:03 +0700

    Update materi_pengertian.py

--- bot_analytics/commands/materi_pengertian.py
+++ bot_analytics/commands/materi_pengertian.py
@@ -5,6 +5,7 @@ from tinydb import TinyDB, Query
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 
 from bot_analytics.command import TELEGRAM_BOT_COMMANDS
+from programs.message import editMessage
 from config import TOKEN
 
 
@@ -17,20 +18,10 @@ def command_handler(sent_message, user_message):
     message_text = user_message['text']
     user_message_id = user_message['message_id']
 
-    try:
-        bot.editMessageText((chat_id, message_id), "Disini kita akan belajar Materi Pengertian tentang bolero/rompi berikut, pilih salah satu👇", reply_markup = InlineKeyboardMarkup(inline_keyboard=[
+    editMessage(chat_id, message_id, "Disini kita akan belajar Materi Pengertian tentang bolero/rompi berikut, pilih salah satu👇", reply_markup = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="> Sejarah Bolero/Rompi <",callback_data='sejarah')],
         [InlineKeyboardButton(text="> Pengertian Bolero/Rompi <",callback_data='pengertian')],
         [InlineKeyboardButton(text="> Perbedaan Bolero/Rompi <",callback_data='perbedaan')],
         [InlineKeyboardButton(text="> Desain Bolero/Rompi <",callback_data='desain')],
         [InlineKeyboardButton(text="> Macam Bolero/Rompi <",callback_data='macam')]
     ]))
-    except:
-        bot.sendMessage(chat_id, "Disini kita akan belajar Materi Pengertian tentang bolero/rompi berikut, pilih salah satu👇", reply_markup = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="> Sejarah Bolero/Rompi <",callback_data='sejarah')],
-        [InlineKeyboardButton(text="> Pengertian Bolero/Rompi <",callback_data='pengertian')],
-        [InlineKeyboardButton(text="> Perbedaan Bolero/Rompi <",callback_data='perbedaan')],
-        [InlineKeyboardButton(text="> Desain Bolero/Rompi <",callback_data='desain')],
-        [InlineKeyboardButton(text="> Macam Bolero/Rompi <",callback_data='macam')]
-    ]))
-        pass
