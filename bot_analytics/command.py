18b7c41e016714feeb2c90162187d3bc56533a2c
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-20 09:38:29 +0700

    Update command.py

--- bot_analytics/command.py
+++ bot_analytics/command.py
@@ -3,8 +3,10 @@ import importlib
 TELEGRAM_BOT_COMMANDS = {
     "/start": "bot_analytics.commands.start",
     "/tanya": "bot_analytics.commands.tanya",
-    "/cari": "bot_analytics.commands.cari"
+    "/cari": "bot_analytics.commands.cari",
+    "Pengertian✍️️": "bot_analytics.commands.materi_pengertian",
+    "Menyiapkan Ukuran📏": "bot_analytics.commands.materi_ukuran",
+    "Pola📐": "bot_analytics.commands.materi_pola",
+    "Bahan🧵": "bot_analytics.commands.materi_bahan",
+    "Vidio Tutorial⏸": "bot_analytics.commands.materi_vidio"
 }
-
-def addCommand():
-    return "Wip"
