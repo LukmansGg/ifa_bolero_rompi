87185755dad437e50cdaad17b6587acff0aae512
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-20 11:39:39 +0700

    Update callbacks

---
+++ bot_analytics/callbacks/materi_page_sejarah.py
@@ -0,0 +1,3 @@
+
+
+jelaskan sejarah bolero/rompi secara singkat menurut beberapa sumber ini https://www.womanindonesia.co.id/sejarah-jaket-bolero-di-industri-fashion/  dan https://fitinline.com/article/read/vest/ dan https://kumparan.com/hijab-lifestyle/mengulik-sejarah-rompi-yang-sudah-populer-sejak-abad-ke-17-1y1JZmTp0ZL
