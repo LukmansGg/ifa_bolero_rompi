a0c8529ef681c077d718a31ccfd87446530a637d
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-20 12:04:07 +0700

    Update materi_page_sejarah.py

--- bot_analytics/callbacks/materi_page_sejarah.py
+++ bot_analytics/callbacks/materi_page_sejarah.py
@@ -22,7 +22,7 @@ def callback_handler(msg):
 
     keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Regenerate', callback_data = 'regenerate')]])
 
-    question = 'jelaskan sejarah bolero/rompi secara singkat menurut beberapa sumber ini https://www.womanindonesia.co.id/sejarah-jaket-bolero-di-industri-fashion/  dan https://fitinline.com/article/read/vest/ dan https://kumparan.com/hijab-lifestyle/mengulik-sejarah-rompi-yang-sudah-populer-sejak-abad-ke-17-1y1JZmTp0ZL'
+    question = 'jelaskan sejarah bolero/rompi secara singkat menurut beberapa sumber ini https://www.womanindonesia.co.id/sejarah-jaket-bolero-di-industri-fashion/  dan https://fitinline.com/article/read/vest/ dan https://kumparan.com/hijab-lifestyle/mengulik-sejarah-rompi-yang-sudah-populer-sejak-abad-ke-17-1y1JZmTp0ZL, susun ulang sumber sumber di atas lagi'
     response = gpt3(question)
     # Edit pesan asli dengan respon
     bot.editMessageText((from_id, original_message_id), response, reply_markup=keyboard)
