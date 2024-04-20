4433b5b5d080b54202d222fb1797f8e80ddbeef8
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-20 14:51:58 +0700

    Update materi_back_pengertian.py

--- bot_analytics/callbacks/materi_back_pengertian.py
+++ bot_analytics/callbacks/materi_back_pengertian.py
@@ -20,11 +20,10 @@ def callback_handler(msg):
     query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
     original_message_id = msg['message']['message_id']
 
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Regenerate', callback_data = 'regenerate')]])
-
-    question = 'jelaskan materi pengertian bolero dan rompi, baik dari perbedaan, asal usul, desain, dll'
-    response = gpt3(question)
-    # Edit pesan asli dengan respon
-    bot.editMessageText((from_id, original_message_id), f'-Pengertian Bolero dan Rompi-\n\nMenurut beberapa sumber:\n\n1.Url\n{response}', reply_markup=keyboard)
-    db.insert({'chat_id': from_id, 'message_id': original_message_id, 'question': question, 'answer': response})
-  
+    bot.editMessageText((from_id, original_message_id), "Disini kita akan belajar Materi Pengertian tentang bolero/rompi berikut, pilih salah satu👇", InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="> Sejarah Bolero/Rompi <",callback_data='sejarah')],
+        [InlineKeyboardButton(text="> Pengertian Bolero/Rompi <",callback_data='pengertian')],
+        [InlineKeyboardButton(text="> Perbedaan Bolero/Rompi <",callback_data='perbedaan')],
+        [InlineKeyboardButton(text="> Desain Bolero/Rompi <",callback_data='desain')],
+        [InlineKeyboardButton(text="> Macam Bolero/Rompi <",callback_data='macam')]
+    ])) 
