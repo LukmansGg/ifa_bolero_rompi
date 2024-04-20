c654875cf8bd41728bf64b6f469bbfb0ff0f7aa9
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-21 06:32:12 +0700

    Update materi_pola_bolero_besar.py

--- bot_analytics/commands/materi_pola_bolero_besar.py
+++ bot_analytics/commands/materi_pola_bolero_besar.py
@@ -24,4 +24,4 @@ def command_handler(sent_message, user_message):
 
 
     editMessage(chat_id, message_id, f"-- <b>Membuat Pola Bolero</b> --\n:[ Pola Bolero Skala Besar ]:\n\n{answer}\n\n[ <b>https://adabitaylor.blogspot.com/2016/03/pola-bolero-sederhana.html?m=1</b> ]", InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Re-Generate",callback_data='regenerate')]]))
-    db.insert({'chat_id': chat_id, 'message_id': message_id, 'question': 'jelaskan cara membuat pola bolero dengan skala besar, dan jelaskan berapa itu skala besar', 'answer': answer})
+    db.insert({'chat_id': chat_id, 'message_id': message_id, 'question': 'jelaskan cara membuat pola bolero dengan skala besar, dan jelaskan berapa itu skala besar', 'answer': answer, 'header': '-- <b>Membuat Pola Bolero</b> --\n:[ Pola Bolero Skala Besar ]:\n\n'})
