b712a530ce9ccdc446fdc1338a42ff20f2e49a08
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-20 14:50:40 +0700

    Update materi_back_pengertian.py

--- bot_analytics/callbacks/materi_back_pengertian.py
+++ bot_analytics/callbacks/materi_back_pengertian.py
@@ -1,1 +1,30 @@
-p
+import os
+import sys
+import importlib
+from tinydb import TinyDB, Query
+from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+
+from config import TOKEN
+from programs.gpt import gpt3
+import telepot
+from programs.message import answeringMessage
+from bot_analytics.command import TELEGRAM_BOT_COMMANDS
+
+#sys.path.append('commands')
+
+bot = telepot.Bot(TOKEN)
+db = TinyDB('chat_data.json')
+
+
+def callback_handler(msg):
+    query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
+    original_message_id = msg['message']['message_id']
+
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Regenerate', callback_data = 'regenerate')]])
+
+    question = 'jelaskan materi pengertian bolero dan rompi, baik dari perbedaan, asal usul, desain, dll'
+    response = gpt3(question)
+    # Edit pesan asli dengan respon
+    bot.editMessageText((from_id, original_message_id), f'-Pengertian Bolero dan Rompi-\n\nMenurut beberapa sumber:\n\n1.Url\n{response}', reply_markup=keyboard)
+    db.insert({'chat_id': from_id, 'message_id': original_message_id, 'question': question, 'answer': response})
+  
