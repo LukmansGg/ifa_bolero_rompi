c69bd68399d0eebef71b26dd89613e778f185d7b
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-20 10:23:07 +0700

    Update command.py

--- bot_analytics/command.py
+++ bot_analytics/command.py
@@ -4,9 +4,9 @@ TELEGRAM_BOT_COMMANDS = {
     "/start": "bot_analytics.commands.start",
     "/tanya": "bot_analytics.commands.tanya",
     "/cari": "bot_analytics.commands.cari",
-    "Pengertian✍️️": "bot_analytics.commands.materi_pengertian"
-    #"Menyiapkan Ukuran📏": "bot_analytics.commands.materi_ukuran",
-    #"Pola📐": "bot_analytics.commands.materi_pola",
+    "Pengertian✍️️": "bot_analytics.commands.materi_pengertian",
+    "Menyiapkan Ukuran📏": "bot_analytics.commands.materi_ukuran",
+    "Pola📐": "bot_analytics.commands.materi_pola"
     #"Bahan🧵": "bot_analytics.commands.materi_bahan",
     #"Vidio Tutorial⏸": "bot_analytics.commands.materi_vidio"
 }
