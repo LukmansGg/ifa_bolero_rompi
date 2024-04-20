52f578d1e90953dcafcf27c737209fb7592c561d
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-20 10:37:52 +0700

    Update gpt.py

--- programs/gpt.py
+++ programs/gpt.py
@@ -8,20 +8,20 @@ def gpt3(prompt):
     if response.choices[0].message.content == "当前地区当日额度已消耗完, 请尝试更换网络环境":
         return "Maaf sedang terjadi kesalahan pada sistem ai kami, silakan coba lagi"
     else:
-        return response.choices[0].message.content
+        return response.choices[0].message.content + "\n\n\n[Powered by OpenAI]"
   except Exception as e:
     return "Terjadi kesalahan: {}".format(e)
 
 def gpt4(prompt):
   try:
     response = gpt_client.chat.completions.create(model='gpt-4-turbo', messages=[{'role': 'user', 'content': prompt}])
-    return response.choices[0].message.content
+    return response.choices[0].message.content + "\n\n\n[Powered by OpenAI 4]"
   except Exception as e:
     return "Terjadi kesalahan: {}".format(e)
 
 def gpt(model, prompt):
   try:
     response = gpt_client.chat.completions.create(model=model, messages=[{'role': 'user', 'content': prompt}])
-    return response.choices[0].message.content
+    return response.choices[0].message.content + "\n\n\n[Powered by OpenAI]"
   except Exception as e:
     return "Terjadi kesalahan: {}".format(e)
