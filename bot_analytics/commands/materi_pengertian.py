03f343d00b53df0e93e7e25d0d8fd0e7452946ab
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-20 09:44:45 +0700

    Update materi_pengertian.py

--- bot_analytics/commands/materi_pengertian.py
+++ bot_analytics/commands/materi_pengertian.py
@@ -17,10 +17,20 @@ def command_handler(sent_message, user_message):
     message_text = user_message['text']
     user_message_id = message['message_id']
 
-    bot.sendMessage(chat_id, "Disini kita akan belajar Materi Pengertian tentang bolero/rompi berikut, pilih salah satu👇", reply_markup = InlineKeyboardMarkup(inline_keyboard=[
+    try:
+        bot.editMessageText((chat_id, message_id), "Disini kita akan belajar Materi Pengertian tentang bolero/rompi berikut, pilih salah satu👇", reply_markup = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="> Sejarah Bolero/Rompi <",callback_data='sejarah')],
         [InlineKeyboardButton(text="> Pengertian Bolero/Rompi <",callback_data='pengertian')],
         [InlineKeyboardButton(text="> Perbedaan Bolero/Rompi <",callback_data='perbedaan')],
         [InlineKeyboardButton(text="> Desain Bolero/Rompi <",callback_data='desain')],
         [InlineKeyboardButton(text="> Macam Bolero/Rompi <",callback_data='macam')]
     ]))
+    except:
+        bot.sendMessage(chat_id, "Disini kita akan belajar Materi Pengertian tentang bolero/rompi berikut, pilih salah satu👇", reply_markup = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="> Sejarah Bolero/Rompi <",callback_data='sejarah')],
+        [InlineKeyboardButton(text="> Pengertian Bolero/Rompi <",callback_data='pengertian')],
+        [InlineKeyboardButton(text="> Perbedaan Bolero/Rompi <",callback_data='perbedaan')],
+        [InlineKeyboardButton(text="> Desain Bolero/Rompi <",callback_data='desain')],
+        [InlineKeyboardButton(text="> Macam Bolero/Rompi <",callback_data='macam')]
+    ]))
+        pass
