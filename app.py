3c3657e4dbb32f5dcc0d5d735cd82133cc1312f2
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-21 07:27:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,8 @@ from flask import Flask, request
 import telepot
 from googlesearch import search
 from telepot.loop import MessageLoop
-from bot_analytics.operator import handle_message, handle_callback
+from bot_analytics.operator import handle_message, handle_callback, handle_voice_message
 from config import TOKEN
-
-sys.path.append('programs')
 
 app = Flask(__name__)
 
@@ -18,5 +16,5 @@ def home():
 
 if __name__ == '__main__':
     bot = telepot.Bot(TOKEN)
-    MessageLoop(bot, {'chat': handle_message, 'callback_query': handle_callback}).run_as_thread()
+    MessageLoop(bot, {'chat': handle_message, 'callback_query': handle_callback, 'voice': handle_voice_message}).run_as_thread()
     app.run(host='0.0.0.0', port=8000)
