4f37d5ae83e341e81cf4a5c9f4dbc3f6aadef116
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-24 19:01:00 +0700

    Update start.py

--- bot_analytics/commands/start.py
+++ bot_analytics/commands/start.py
@@ -1,42 +1,31 @@
+import time
 import telepot
 from tinydb import TinyDB
-from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
-from config import TOKEN
-from programs.message import editMessage
 
 bot = telepot.Bot(TOKEN)
 db = TinyDB('welcome_chat.json')
 
 def command_handler(sent_message, message):
     chat_id = sent_message['chat']['id']
-    message_id = sent_message['message_id']
 
-    # Memeriksa apakah ada entri dalam basis data
-    if db:
-        # Mengambil data terakhir dari database
-        last_entry = db.all()[-1]
+    # Menghasilkan ID unik berdasarkan timestamp
+    unique_id = int(time.time())
 
-        # Memeriksa apakah data terakhir adalah teks selamat datang
+    # Memeriksa apakah data terakhir adalah teks selamat datang
+    last_entry = db.all()
+    if last_entry:
+        last_entry = last_entry[-1]
         if last_entry.get('is_welcome', False):
             bot.deleteMessage((chat_id, last_entry['message_id']))
             bot.deleteMessage((chat_id, last_entry['sent_message_id']))
 
-    editMessage(chat_id, message_id, "Selamat Datang di @Ifa_bolero_dan_rompi_bot\nDisini kita dapat belajar bersama berbagai Hal tentang Bolero/Rompi😁👍\n")
-
-    # Mengirim pesan untuk dipilih
-    welcome_chat = bot.sendMessage(chat_id, "Pilih salah satu materi disini👇.", reply_markup=ReplyKeyboardMarkup(
-        keyboard=[
-            [KeyboardButton(text="Pengertian✍️️")],
-            [KeyboardButton(text="Menyiapkan Ukuran📏")],
-            [KeyboardButton(text="Pola📐")],
-            [KeyboardButton(text="Bahan🧵")],
-            [KeyboardButton(text="Vidio Tutorial▶️")]
-        ],
-        resize_keyboard=True
-    ))
+    # Menampilkan pesan selamat datang
+    message_id = sent_message['message_id']
+    bot.sendMessage(chat_id, "Selamat Datang!")
 
     # Menyimpan pesan selamat datang saat ini
-    db.insert({'chat_id': chat_id, 'message_id': message_id, 'sent_message_id': welcome_chat['message_id'], 'is_welcome': True})
+    welcome_chat = bot.sendMessage(chat_id, "Pilih salah satu materi disini👇.")
+    db.insert({'id': unique_id, 'chat_id': chat_id, 'message_id': message_id, 'sent_message_id': welcome_chat['message_id'], 'is_welcome': True})
 
     # Menghapus pesan dari pengguna
     try:
