5229e8378ccf9e65f099a53909682ac1258a8ff6
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-24 16:57:50 +0700

    Update start.py

--- bot_analytics/commands/start.py
+++ bot_analytics/commands/start.py
@@ -1,17 +1,28 @@
 import telepot
-from tinydb import TinyDB
+from tinydb import TinyDB, Query
 from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
 from bot_analytics.command import TELEGRAM_BOT_COMMANDS
 from config import TOKEN
 from programs.message import editMessage
 
 bot = telepot.Bot(TOKEN)
-db = TinyDB('chat_data.json')
+db = TinyDB('welcome_chat.json')
 
 def command_handler(sent_message, message):
     chat_id = sent_message['chat']['id']
+
+    # Mengambil data terakhir dari database
+    last_entry = db.all()[-1]
+
+    # Memeriksa apakah data terakhir adalah teks selamat datang
+    if last_entry.get('is_welcome', False):
+        bot.deleteMessage((chat_id, last_entry['message_id']))
+
     message_id = sent_message['message_id']
     editMessage(chat_id, message_id, "Selamat Datang di @Ifa_bolero_dan_rompi_bot\nDisini kita dapat belajar bersama berbagai Hal tentang Bolero/Rompi😁👍\n")
+
+    # Menyimpan pesan selamat datang saat ini
+    db.insert({'chat_id': chat_id, 'message_id': message_id, 'is_welcome': True})
 
     bot.sendMessage(chat_id, "Pilih salah satu materi disini👇.", reply_markup=ReplyKeyboardMarkup(
         keyboard=[
