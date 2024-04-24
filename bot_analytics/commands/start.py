c1358064c7c16e937c68b9d9f49cec4071dfb4fa
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-24 17:41:47 +0700

    Update start.py

--- bot_analytics/commands/start.py
+++ bot_analytics/commands/start.py
@@ -1,7 +1,6 @@
 import telepot
-from tinydb import TinyDB, Query
+from tinydb import TinyDB
 from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
-from bot_analytics.command import TELEGRAM_BOT_COMMANDS
 from config import TOKEN
 from programs.message import editMessage
 
@@ -17,14 +16,14 @@ def command_handler(sent_message, message):
     # Memeriksa apakah data terakhir adalah teks selamat datang
     if last_entry.get('is_welcome', False):
         bot.deleteMessage((chat_id, last_entry['message_id']))
+        bot.deleteMessage((chat_id, last_entry['sent_message_id']))
 
+    # Menampilkan pesan selamat datang
     message_id = sent_message['message_id']
     editMessage(chat_id, message_id, "Selamat Datang di @Ifa_bolero_dan_rompi_bot\nDisini kita dapat belajar bersama berbagai Hal tentang Bolero/Rompi😁👍\n")
 
-    # Menyimpan pesan selamat datang saat ini
-    db.insert({'chat_id': chat_id, 'message_id': message_id, 'is_welcome': True})
-
-    bot.sendMessage(chat_id, "Pilih salah satu materi disini👇.", reply_markup=ReplyKeyboardMarkup(
+    # Mengirim pesan untuk dipilih
+    welcome_chat = bot.sendMessage(chat_id, "Pilih salah satu materi disini👇.", reply_markup=ReplyKeyboardMarkup(
         keyboard=[
             [KeyboardButton(text="Pengertian✍️️")],
             [KeyboardButton(text="Menyiapkan Ukuran📏")],
@@ -35,6 +34,10 @@ def command_handler(sent_message, message):
         resize_keyboard=True
     ))
 
+    # Menyimpan pesan selamat datang saat ini
+    db.insert({'chat_id': chat_id, 'message_id': message_id, 'sent_message_id': welcome_chat['message_id'], 'is_welcome': True})
+
+    # Menghapus pesan dari pengguna
     try:
         user_message_id = message.get('message_id')
         bot.deleteMessage((chat_id, user_message_id))
