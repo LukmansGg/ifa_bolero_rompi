a0cf54f7f7a6bbdb7b2b6e8c9084708cf3d32d75
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-24 20:46:24 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TOKEN = '6402398667:AAFEqH80RtZA1R6QGsFL3wMB_0djGQgX5L4' # < -- place your telegram bot token here
+TOKEN = '6402398667:AAGOAg1kqligdhgxT4DjzQkeNJOXqBf5qYM' # < -- place your telegram bot token here
 
 AAI_TOKEN = 'c977bf8599f84ace901471a69180904c'
