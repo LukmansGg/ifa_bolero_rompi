54225d6a71efc72055ccfcdf53a28dbad8f8ed26
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-24 19:51:07 +0700

    Update previous_search.py

--- bot_analytics/callbacks/previous_search.py
+++ bot_analytics/callbacks/previous_search.py
@@ -34,7 +34,7 @@ def callback_handler(msg):
             
             keyboard = InlineKeyboardMarkup(inline_keyboard=[keyboard_buttons])
 
-            editMessage(from_id, chat_message_id, f"<b>[{next_page_value - 1}/10] Hasil Pencarian: [{title}]</b>\n\n{description}n\n[ <b>{url}</b> ]\n\npowered by [googlesearch]", keyboard)
+            editMessage(from_id, chat_message_id, f"<b>[{next_page_value - 1}/20] Hasil Pencarian: [{title}]</b>\n\n{description}n\n[ <b>{url}</b> ]\n\npowered by [googlesearch]", keyboard)
             chat_db.update({'link_page': next_page_value}, query.message_id == chat_message_id)
             
     else:
