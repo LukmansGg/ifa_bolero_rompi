2e036ec056a90222cdb214988f74dbfe9fa59b79
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2024-04-25 20:27:25 +0700

    Update start.py

--- bot_analytics/commands/start.py
+++ bot_analytics/commands/start.py
@@ -1,11 +1,9 @@
 import telepot
-import time
-from tinydb import TinyDB, Query
+import uuid
+from tinydb import TinyDB
 from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
-from bot_analytics.command import TELEGRAM_BOT_COMMANDS
 from config import TOKEN
 from programs.message import editMessage
-
 
 bot = telepot.Bot(TOKEN)
 db = TinyDB('welcome_chat.json')
@@ -13,27 +11,22 @@ db = TinyDB('welcome_chat.json')
 def command_handler(sent_message, message):
     chat_id = sent_message['chat']['id']
     message_id = sent_message['message_id']
-    # Menghasilkan ID unik berdasarkan timestamp
-    unique_id = int(time.time())
+    
+    # Generate a unique ID using UUID
+    unique_id = str(uuid.uuid4())
 
-    # Memeriksa apakah data terakhir adalah teks selamat datang
     last_entry = db.all()
     if last_entry:
         last_entry = last_entry[-1]
         if last_entry.get('is_welcome', False):
             try:
                 bot.deleteMessage((chat_id, last_entry['message_id']))
-            except telepot.exception.TelegramError as e:
-                pass
-
-            try:
                 bot.deleteMessage((chat_id, last_entry['sent_message_id']))
             except telepot.exception.TelegramError as e:
                 pass
 
     editMessage(chat_id, message_id, "Selamat Datang di @Ifa_bolero_dan_rompi_bot\nDisini kita dapat belajar bersama berbagai Hal tentang Bolero/Rompi😁👍\n")
 
-    # Mengirim pesan untuk dipilih
     welcome_chat = bot.sendMessage(chat_id, "Pilih salah satu materi disini👇.", reply_markup=ReplyKeyboardMarkup(
         keyboard=[
             [KeyboardButton(text="Pengertian✍️️")],
@@ -46,7 +39,6 @@ def command_handler(sent_message, message):
     ))
     db.insert({'id': unique_id, 'chat_id': chat_id, 'message_id': message_id, 'sent_message_id': welcome_chat['message_id'], 'is_welcome': True})
 
-    # Menghapus pesan dari pengguna
     try:
         user_message_id = message.get('message_id')
         bot.deleteMessage((chat_id, user_message_id))
