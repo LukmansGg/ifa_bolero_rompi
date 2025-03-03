4be0e4112d140240c0aae4878a24fd2f4fbb0894
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2025-03-03 13:15:52 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 TOKEN = '6507300714:AAFS7sTEk9o2UxU8cskXQ5gq2q9jGTju7u8' # < -- place your telegram bot token here
-WEBHOOK_URL = 'technical-joann-lukman.koyeb.app/ifaa'
+WEBHOOK_URL = 'technical-joann-lukman.koyeb.app'
 PORT = 8000
 AAI_TOKEN = 'c977bf8599f84ace901471a69180904c'
