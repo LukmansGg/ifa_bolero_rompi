5da7c195b77bca011cd011907ed3b6669bd89637
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2025-03-03 14:52:39 +0700

    Update cari.py

--- bot_analytics/commands/cari.py
+++ bot_analytics/commands/cari.py
@@ -49,4 +49,4 @@ def command_handler(sent_message, message):
             editMessage(chat_id, message_id, f"[1/20]\n<b>Hasil Pencarian🔎:\n [{title}]</b>\n\n{description}\n\n[ <b>{url}</b> ]\n\npowered by [googlesearch]", InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="next",callback_data='searchnext')]]))
             
         else:
-            editMessage(chat_id, message_id, f"Tidak ditemukan hasil pencarian untuk <b>[ search-id: {query} ]</b> yang diberikan")
+            editMessage(chat_id, message_id, f"Tidak ditemukan hasil pencarian untuk *[ search-id: {query} ]* yang diberikan")
