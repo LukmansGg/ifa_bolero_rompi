a62acbdf33a6f73e8118cc2dc73b8a9de1c164f5
LukmansGg <133926281+LukmansGg@users.noreply.github.com>
2025-03-03 14:17:37 +0700

    Update gpt.py

--- programs/gpt.py
+++ programs/gpt.py
@@ -4,7 +4,7 @@ from g4f.client import Client
 gpt_client = Client()
 def gpt3(prompt):
   try:
-    response = gpt_client.chat.completions.create(model='gpt-3.5-turbo', messages=[{'role': 'user', 'content': prompt}])
+    response = gpt_client.chat.completions.create(model='gpt-4o-mini', messages=[{'role': 'user', 'content': prompt}])
     if response.choices[0].message.content == "当前地区当日额度已消耗完, 请尝试更换网络环境":
         return "Maaf sedang terjadi kesalahan pada sistem ai kami, silakan coba lagi"
     else:
@@ -14,14 +14,14 @@ def gpt3(prompt):
 
 def gpt4(prompt):
   try:
-    response = gpt_client.chat.completions.create(model='gpt-4-turbo', messages=[{'role': 'user', 'content': prompt}])
+    response = gpt_client.chat.completions.create(model='gpt-4', messages=[{'role': 'user', 'content': prompt}])
     return response.choices[0].message.content + "\n\n\n[Powered by OpenAI 4]"
   except Exception as e:
     return "Terjadi kesalahan: {}".format(e)
 
 def gpt(model, prompt):
   try:
-    response = gpt_client.chat.completions.create(model=model, messages=[{'role': 'user', 'content': prompt}])
+    response = gpt_client.chat.completions.create(model='gpt-4o-mini', messages=[{'role': 'user', 'content': prompt}])
     return response.choices[0].message.content + "\n\n\n[Powered by OpenAI]"
   except Exception as e:
     return "Terjadi kesalahan: {}".format(e)
